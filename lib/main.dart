f86bdf5b9ac55ff83622d7d68514280fe490e474
Siddhartha Anand <imsidanand@gmail.com>
2019-12-30 15:02:33 +0530

    Welcome Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
-void main(){
-  
+import 'package:flutter/material.dart';
+import './ui/welocome_home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Welcome App',
+    home: new Welcome(),
+  ));
 }
