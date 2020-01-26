858a110b9b48b67f01d393616c121b6f5c3dbe92
Siddhartha Anand <imsidanand@gmail.com>
2020-01-26 21:59:56 +0530

    changed display text

--- lib/ui/welocome_home.dart
+++ lib/ui/welocome_home.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-class Welcome extends StatelessWidget{
+
+class Welcome extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return new Material(
-      color: Colors.blue,
-      child: new Center(
-        child: new Text(
-          "Welcome baby",
-          textDirection: TextDirection.ltr,
-          style: new TextStyle(
-            fontWeight: FontWeight.w900,
-            fontStyle: FontStyle.italic,
-            fontSize: 34.5
-          ),
-        ),
+    return Container(
+      color: Colors.yellow,
+      child: Center(
+
+        child: Text("This is my first flutter app"),
       ),
     );
   }
